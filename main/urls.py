26c859e8383a9630127b185521e568b24886daa4
yiivan <yiivan@hotmail.com>
2016-08-23 14:46:08 -0700

    ajax

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^index/$', views.index),
+    url(r'^json/$', views.ajax),
+    url(r'^$', views.index2),
+
 ]
--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ import json
 import glob
 from django.conf import settings
 from django.shortcuts import render
+from django.http import HttpResponse
 # import pdb
 
 # Create your views here.
@@ -23,3 +24,19 @@ def index(request):
         # pdb.set_trace()
 
     return render(request, 'index.html', {'data': files_data})
+
+def ajax(request):
+    path = os.path.join(settings.FILE_PATH)
+    files = glob.glob(path)
+    files_data = []
+    for file in files:
+        opener = open(file, 'r')
+        json_data = opener.read()
+        file_data = json.loads(json_data.replace("\'", '"'))
+        files_data += file_data
+    #disconnects the file from the variable
+        opener.close()
+    return HttpResponse(json.dumps(files_data), content_type = "application/json")
+
+def index2(request):
+    return render(request, 'index2.html')
