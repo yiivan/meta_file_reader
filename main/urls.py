e54a77cbae7c3b90db318182786a1847bb2483f7
yiivan <yiivan@hotmail.com>
2016-08-23 21:09:10 -0700

    remove old HTML

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index/$', views.index),
     url(r'^json/$', views.ajax),
-    url(r'^$', views.index2),
-
+    url(r'^$', views.index),
 ]
--- main/views.py
+++ main/views.py
@@ -4,26 +4,8 @@ import glob
 from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse
-# import pdb
 
 # Create your views here.
-
-def index(request):
-    #opens the json files and saves the raw contents and deserialize it
-    path = os.path.join(settings.FILE_PATH)
-    files = glob.glob(path)
-    files_data = []
-    for file in files:
-        opener = open(file, 'r')
-        json_data = opener.read()
-        file_data = json.loads(json_data.replace("\'", '"'))
-        files_data += file_data
-    #disconnects the file from the variable
-        opener.close()
-
-        # pdb.set_trace()
-
-    return render(request, 'index.html', {'data': files_data})
 
 def ajax(request):
     path = os.path.join(settings.FILE_PATH)
@@ -34,9 +16,10 @@ def ajax(request):
         json_data = opener.read()
         file_data = json.loads(json_data.replace("\'", '"'))
         files_data += file_data
-    #disconnects the file from the variable
+
+        #disconnects the file from the variable
         opener.close()
     return HttpResponse(json.dumps(files_data), content_type = "application/json")
 
-def index2(request):
-    return render(request, 'index2.html')
+def index(request):
+    return render(request, 'index.html')
